b1c818acfc376678cdba5f389cb26fad80121c0b
Lena Damisi <Lena.damisi12@gmail.com>
2024-09-24 13:26:30 +0200

    Added Price Class

---
+++ Airport Ticket Booking System/Price.cs
@@ -0,0 +1,20 @@
+﻿namespace Airport_Ticket_Booking_System.General;
+
+public class Price
+{
+    public double TicketPrice { get; set; }
+    public Currency Currency { get; set; }
+
+    public override string ToString()
+    {
+        return $"{TicketPrice} {Currency}";
+    }
+    public Price() { }
+    public Price(double price, Currency currency)
+    {
+        TicketPrice = price;
+        Currency = currency;
+    }
+}
+
+
