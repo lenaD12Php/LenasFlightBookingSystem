fdee5485354e4536a648e6a0586d4c76d40bf191
Lena Damisi <Lena.damisi12@gmail.com>
2024-10-13 13:56:07 +0300

    Created a passenger record with suitable properities

---
+++ Airport Ticket Booking System/BookingSystemModels/Passenger.cs
@@ -0,0 +1,81 @@
+﻿namespace Airport_Ticket_Booking_System;
+
+
+public record Passenger(string ID, string FirstName, string LastName, string Email, string Phone, PassengerType PassengerType, IInputValidation Validation)
+{
+    private string _firstName;
+    private string _lastName;
+    private string _phone;
+    private string _email;
+    private PassengerType _type;
+
+    public string FirstName
+    {
+        get => _firstName;
+        init
+        {
+            if (Validation.IsValidName(value) != ValidationErrorType.None)
+            {
+                throw new ArgumentException("Invalid name format.");
+            }
+            _firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get => _lastName;
+        init
+        {
+            if (Validation.IsValidName(value) != ValidationErrorType.None)
+            {
+                throw new ArgumentException("Invalid name format.");
+            }
+            _lastName = value;
+        }
+    }
+
+    public string Email
+    {
+        get => _email;
+        init
+        {
+            if (Validation.IsValidEmail(value) != ValidationErrorType.None)
+            {
+                throw new ArgumentException("Invalid email format.");
+            }
+            _email = value;
+        }
+    }
+
+    public string Phone
+    {
+        get => _phone;
+        init
+        {
+
+            if (Validation.IsValidPhoneNumber(value) != ValidationErrorType.None)
+            {
+                throw new ArgumentException("Invalid phone number format. It should only contain digits and optionally start with '+'.");
+            }
+            _phone = value;
+        }
+    }
+
+    public PassengerType PassengerType
+    {
+        get => _type;
+        init
+        {
+            if (!Enum.IsDefined(typeof(PassengerType), value))
+            {
+                throw new ArgumentException("Invalid passenger type.");
+            }
+            _type = value;
+        }
+    }
+}
+
+
+
+
